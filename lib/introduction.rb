e6f628cc28b156568494eab7d2d4623aea5d96a7
Marissa Nolan <ritz1484@cs.com>
2021-01-12 01:02:44 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -2,19 +2,19 @@ def introduction (name)
   puts "Hi my name is #{name}."
 end
 
-introduction "Marissa"
+introduction ("Marissa")
 
 def introduction_with_language (name, language)
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
 
-introduction_with_language "Marissa"
+introduction_with_language ("Marissa", "Python")
 
 def introduction_with_language_optional (name, language = "Ruby")
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
 
-introduction_with_language_optional
+introduction_with_language_optional ("Marissa")
 
 
 
